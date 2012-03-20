0f6cd3a3ca8a85a9f54d40c8460e793ca37b87db
Paul Graham <homosaur@gmail.com>
2012-03-19 23:19:15 -0500

    a bit of interface done - realized i'm going to need handlebars

--- js/script-ck.js
+++ js/script-ck.js
@@ -1,1 +1,1 @@
-/* Author: Paul Edmon Graham*/$(document).ready(function(){});
+/* Author: Paul Edmon Graham*/$(document).ready(function(){$("button#reset").click(function(){$("div#font-staging").empty();$("div#test-types").html('<button id="test-1">Readability - 1 column</button><button id="test-3">Deep stack testing - 3 column</button><button id="test-6">Six boxes</button><button id="test-9">Nine boxes</button>')});$("button#test-1").click(function(){});$("button#test-3").click(function(){});$("button#test-6").click(function(){});$("button#test-9").click(function(){})});
--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
 /* Author: Paul Edmon Graham*/
 
-$(document).ready(function (){
+$(document).ready(function () {
+  
+  $('button#reset').click(function() {
+    $('div#font-staging').empty();
+    $('div#test-types').html('<button id="test-1">Readability - 1 column</button><button id="test-3">Deep stack testing - 3 column</button><button id="test-6">Six boxes</button><button id="test-9">Nine boxes</button>');
+  });
+  
+  
+  
+  $('button#test-1').click(function() {
+    
+  });
+  
+  
+  
+  $('button#test-3').click(function() {
+    
+  });
+  
+  
+  
+  $('button#test-6').click(function() {
+    
+  });
+  
+  
+  
+  $('button#test-9').click(function() {
+    
+  });
+  
+  
   
 });
