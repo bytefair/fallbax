ca7a470e1d2e51366306d1cc91dbd16eae1aea27
Paul Graham <homosaur@gmail.com>
2012-03-21 00:43:16 -0500

    stuck

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 
 $(document).ready(function () {
   
-  var lipsum = "<p>Lorem ipsum dolor sit amet, no has ludus choro conclusionemque. Eu propriae periculis vix, vim sale perfecto salutandi eu. Vis periculis maiestatis cu, ei nec nonumes mediocritatem concludaturque, at debet salutatus conceptam his. Quas probo ridens et has, eius homero intellegat his an, eum harum munere conceptam no.<p>Te reque verear mea. Ad duis eruditi dissentiet vim, per enim viderer tibique eu. Liber aeque voluptatum id mea. Ea alienum omnesque sapientem mel. Cu his soleat eripuit intellegam, ad sit verear minimum invidunt, mea cibo regione sensibus ea.<p>Mei scripta deleniti eu, vim inani vituperata ut, te his feugiat blandit abhorreant. An vitae fuisset ponderum mea, usu cu nostro hendrerit, vis libris pertinacia in. Te nostrum reprimique pri. Ei eam novum laboramus, nam case malorum complectitur et.<p>Vero ullum no vel. At vim deserunt postulant. Omittam sadipscing eos at, ponderum menandri philosophia est ea. Est ea affert denique, cu cum labitur offendit phaedrum, cu etiam tractatos pri. Meis dissentiet sed ad, sit partem graeco id, ne est feugiat persequeris. Usu erat facete philosophia id.";
-  
   $('button#reset').click(function() {
     $('div#font-staging div').hide();
     $('div#font-staging').hide(200);
@@ -21,7 +19,5 @@ $(document).ready(function () {
     
     $('div.' + event.target.id).show(600);
   });
-  
-  
-  
+
 });
