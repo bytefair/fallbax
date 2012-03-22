b2d055940dc11f435b1867b071e80fe250aea86e
Paul Graham <homosaur@gmail.com>
2012-03-22 11:58:01 -0500

    changes

--- js/script.js
+++ js/script.js
@@ -8,23 +8,22 @@ $(document).ready(function () {
     $('div#font-staging').hide(200);
     $('.UI').show();
     $('div#test-types').show(200);
-    $('div#wrapper').addClass('vertical-align');
   });
   
   $('div#test-types button').click(function(event) {
     $('.UI').hide(200);
     $('div#test-types').hide(200);
-    $('div#wrapper').removeClass('vertical-align');
     $('div#font-staging').show();
     $('div#font-staging div').hide();
-    var classNum = parseInt(event.target.id, 10);
     $('div.test-row').show();
-    
+
+    var classNum = parseInt(event.target.id, 10);
     if (classNum === 3 || classNum === 6 || classNum === 9 ) {
         $('div.test-3-column').show();
     }
     
-    $('div.test-' + classNum).show(400);
+    $('div#test-' + classNum).show(400);
+    $('div#test-' + classNum + '-controls').show(400);
   });
 
 });
