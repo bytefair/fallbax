31757346fde1853a929d90cdb5563d1c3fb0e041
Paul Graham <homosaur@gmail.com>
2012-04-01 16:55:40 -0500

    modified a bunch of small issues

--- js/script.js
+++ js/script.js
@@ -2,34 +2,6 @@
 
 $(document).ready(function () {
 
-  $('button#reset').click(function() {
-    $('div.test-row').hide();
-    $('div#font-staging div').hide();
-    $('div#font-staging').hide(200);
-    $('.UI').show();
-    $('div#test-types').show(200);
-  });
-  
-  $('div#test-types button').click(function(event) {
-    $('.UI').hide(200);
-    $('div#test-types').hide(200);
-    $('div#font-staging').show();
-    $('div#font-staging div').hide();
-    $('div.test-row').show();
-
-    var classNum = parseInt(event.target.id, 10);
-    if (classNum === 3 || classNum === 6 || classNum === 9 ) {
-        $('div.test-3-column').show();
-    }
-    
-    $('div#test-' + classNum).show(400);
-    $('div#test-' + classNum + '-controls').show(400);
-    $('div#test-' + classNum + '-results').show(400);
-
-  });
-
-
-
 
 
 
@@ -87,6 +59,9 @@ $(document).ready(function () {
     }
     
   });
+
+
+
 
 
 
